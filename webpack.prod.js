e10c21549d7a157de87512dd46130cd15ca6063c
Jose Striedinger <jstriedinger2090@gmail.com>
2021-11-09 01:23:57 -0500

    update webpack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,14 @@
-const merge = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
-const glob = require('glob-all');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const PurifyCSSPlugin = require("purifycss-webpack");
+//const glob = require('glob-all');
+//const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+//const TerserPlugin = require('terser-webpack-plugin');
+//const PurifyCSSPlugin = require("purifycss-webpack");
 
 module.exports = merge(common, {
 	mode: 'production',
-	module: {
+	/*module: {
 	    rules: [
 	      {
 	        test: /\.(gif|png|jpe?g|svg)$/i,
@@ -46,5 +46,5 @@ module.exports = merge(common, {
 	      },
 	      canPrint: true
 	    })    
-	]
+	]*/
 });
